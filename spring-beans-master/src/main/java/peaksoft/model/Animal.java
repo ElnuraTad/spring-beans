0bf7d1e6f26f1628fcfe76edef0a8db2c68a71c0
My Name <tadzhibaevaelnura@gmail.com>
2021-07-16 12:03:25 +0600

    Initial commit

---
+++ spring-beans-master/src/main/java/peaksoft/model/Animal.java
@@ -0,0 +1,32 @@
+package peaksoft.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Animal {
+
+    @Autowired
+    private Cat cat;
+
+    @Autowired
+    private Dog dog;
+
+
+    public Cat getCat() {
+        return cat;
+    }
+
+    public void setCat(Cat cat) {
+        this.cat = cat;
+    }
+
+    public Dog getDog() {
+        return dog;
+    }
+
+    public void setDog(Dog dog) {
+        this.dog = dog;
+    }
+}
